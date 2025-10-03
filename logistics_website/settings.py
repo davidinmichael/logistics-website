fd71cd06fc5691ba30a087bc443a0e595102c402
davidinmichael <davidinmichael@gmail.com>
2025-10-03 09:16:03 +0100

    Mod: Debug statement

--- logistics_website/settings.py
+++ logistics_website/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
 
 ALLOWED_HOSTS = ["*"]
 ENV = os.getenv("ENV", "test")
